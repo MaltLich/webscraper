283314cf27f2b968de3821d49ce335686566fbad
Andrew Novak <andrew.n.novak@gmail.com>
2021-12-07 20:07:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const port = 8000;
 const axios = require("axios");
 const express = require("express");
 const cheerio = require("cheerio");
 
-const app = express;
+const app = express();
+
+app.listen(PORT, () => console.log("server running on PORT ${PORT}"));
